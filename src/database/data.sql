6a9d3dbe02e7254c48281a3c4f88118fdc59cd62
Juliàn Londoño <jlondono433@gmail.com>
2025-10-02 14:55:07 -0500

    mejora de scrpt sql para permisos

--- src/database/data.sql
+++ src/database/data.sql
@@ -12,11 +12,17 @@ INSERT INTO tipo_documentos (nombre, estado) VALUES ('Carnet de Extranjería', T
 INSERT INTO roles (nombre, descripcion, estado) VALUES ('Administrador', 'Administrador del sistema', TRUE);
 INSERT INTO roles (nombre, descripcion, estado) VALUES ('Empleado', 'Empleado del sistema', TRUE);
 INSERT INTO roles (nombre, descripcion, estado) VALUES ('Usuario', 'Usuario del sistema', TRUE);
+INSERT INTO roles (nombre, descripcion, estado) VALUES ('Usuario', 'Usuario del sistema', TRUE);
+INSERT INTO roles (nombre, descripcion, estado) VALUES ('Usuario', 'Usuario del sistema', TRUE);
 
 -- datos para permisos
-INSERT INTO permisos (nombre, descripcion, estado) VALUES ('Crear Usuario', 'Permiso para crear usuarios', TRUE);
-INSERT INTO permisos (nombre, descripcion, estado) VALUES ('Editar Usuario', 'Permiso para editar usuarios', TRUE);
-INSERT INTO permisos (nombre, descripcion, estado) VALUES ('Eliminar Usuario', 'Permiso para eliminar usuarios', TRUE);
+INSERT INTO permisos (nombre, descripcion, estado) VALUES ('gestionar_usuarios', 'Permiso para crear usuarios', TRUE);
+INSERT INTO permisos (nombre, descripcion, estado) VALUES ('gestionar_roles', 'Permiso para gestionar roles', TRUE);
+INSERT INTO permisos (nombre, descripcion, estado) VALUES ('gestionar_permisos', 'Permiso para gestionar permisos', TRUE);
+INSERT INTO permisos (nombre, descripcion, estado) VALUES ('gestionar_centros', 'Permiso para gestionar centros', TRUE);
+INSERT INTO permisos (nombre, descripcion, estado) VALUES ('gestionar_tipo_documentos', 'Permiso para gestionar tipos de documentos', TRUE);
+INSERT INTO permisos (nombre, descripcion, estado) VALUES ('gestionar_cuenta', 'Permiso para gestionar cuenta', TRUE);
+INSERT INTO permisos (nombre, descripcion, estado) VALUES ('ver_dashboard', 'Permiso para ver información global', TRUE);
 
 -- datos para usuarios
 INSERT INTO usuarios (rol_id, tipo_documento_id, documento, nombres, apellidos, correo, telefono, direccion, contrasena, estado) VALUES (1, 1, '1000233255', 'esneider', 'moreno', 'esnedier@emai.com', '31144633456', 'cl 34 # 45 -44', '123456', TRUE);
@@ -32,6 +38,10 @@ INSERT INTO rol_permiso (rol_id, permiso_id) VALUES (1, 3);
 INSERT INTO usuario_permiso (usuario_id, permiso_id) VALUES (1, 1);
 INSERT INTO usuario_permiso (usuario_id, permiso_id) VALUES (1, 2);
 INSERT INTO usuario_permiso (usuario_id, permiso_id) VALUES (1, 3);
+INSERT INTO usuario_permiso (usuario_id, permiso_id) VALUES (1, 4);
+INSERT INTO usuario_permiso (usuario_id, permiso_id) VALUES (1, 5);
+INSERT INTO usuario_permiso (usuario_id, permiso_id) VALUES (1, 6);
+INSERT INTO usuario_permiso (usuario_id, permiso_id) VALUES (1, 7);
 
 -- datos para centro_usuario
 INSERT INTO centro_usuario (centro_id, usuario_id) VALUES (1, 1);
